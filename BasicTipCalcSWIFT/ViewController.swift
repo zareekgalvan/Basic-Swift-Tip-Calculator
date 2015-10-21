363600bc3f9290c9fa41d147b355177129271d94
Zareek Galván <zareek99@gmail.com>
2015-10-21 08:17:03 -0500

    Basic Tip Calculator in iOS with Swift

--- BasicTipCalcSWIFT/ViewController.swift
+++ BasicTipCalcSWIFT/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtMealCost: UITextField!
+    @IBOutlet weak var txtTip: UITextField!
+    
+    @IBOutlet weak var lblTipCalculated: UILabel!
+    
+    var mealCost = ""
+    var tipPercentage = ""
+    var answer : Float = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +29,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnCalclate(sender: UIButton) {
+        calculateTip()
+    }
 
+    @IBAction func btnClear(sender: UIButton) {
+        
+    }
+    
+    func calculateTip() -> Bool {
+        mealCost = txtMealCost.text!
+        tipPercentage = txtTip.text!
+        var fMealCost = (mealCost as NSString).floatValue
+        var fTipPercentage = (tipPercentage as NSString).floatValue
+        
+        answer = fMealCost * (fTipPercentage * 0.01)
+        
+        lblTipCalculated.text = String(answer)
+        
+        return true
+    }
 }
 
